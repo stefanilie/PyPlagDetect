6a886284da5fdeffcd6af9baa36f5dcf9fda2457
Stefan Ilie <ilie.stefan92@yahoo.com>
2018-02-02 17:59:38 +0200

    add sent feature analyser. needs cleaup

---
+++ helper.py
@@ -0,0 +1,108 @@
+from compiler.ast import flatten
+
+class Helper:
+
+    '''
+    Constructor
+    '''
+    def __init__(self, corpus, tagger, stopWords):
+        self.corpus = corpus
+        self.tagger = tagger
+        self.stopWords = stopWords
+
+    '''
+    Returns the exact number of words based on the percentage needed.
+    @param words - [array of strings] words in analysed structure.
+    @param percentage - [double] fraction of resource that needs to be extracted.
+    @return [int] exact number of words that have to be taken into account.
+    '''
+    @staticmethod
+    def get_percentage(words, percentage):
+        return int(percentage * len(words))
+
+
+    '''
+    Intersection of two provided lists.
+    @param list1 - [list]
+    @param list2 - [list]
+    @return [list] intersection of the two lists.
+    '''
+    @staticmethod
+    def get_intersection(list1, list2):
+        list1 = flatten(list1)
+        list2 = flatten(list2)
+
+        return list(set(list1).intersection(set(list2)))
+
+    '''
+    Method obtained from
+    https://github.com/ypeels/nltk-book/blob/master/exercises/2.21-syllable-count.py
+    Calculates syllable count for the provided word.
+    @param word - string representing the word.
+    \=======================DEPRECATED========================/
+    '''
+    @staticmethod
+    def syllables_in_word(word):
+        flat_dict = dict(cmudict.entries())
+        if flat_dict.has_key(word):
+            return len([ph for ph in flat_dict[word] if ph.strip(string.letters)])
+        else:
+            return 0
+
+
+    '''
+    First it calculates the percentage of a feature relative to the bigger one.
+    After this it edits the name of the feature so that it contains
+    the "_percentage" component.
+    @param relative_to - [int] the feature to which we calculate the percentage of
+    the other smaller ones. Example: paragraph_words_count.
+    @param feature - [dict] feature dictionary containing the values to be calculated.
+    '''
+    @staticmethod
+    def get_feature_percentage(relative_to, feature):
+        dict_reply = {}
+        for key, value in feature.iteritems():
+            dict_reply.update({str(key)+"_percentage": 100*value/float(relative_to)})
+        return dict_reply
+
+    '''
+    Computes the Term Frequency (TF).
+    @param term - [string] the term who's TF we're computing.
+    @param tokenized_document - [list string] can be either the sentence,
+    the paragraph, or even the entire document. Based on this we calculate the
+    TF for the according instance.
+    @return [int] value of the TF.
+    '''
+    @staticmethod
+    def compute_TF(term, tokenized_document):
+        return 1 + math.log(tokenized_document.count(term))
+
+    '''
+    Computes the Inverse Term Frequency (IDF) coeficient.
+    IDF = log(Nr of Docs in the Corpus / Nr of Docs in which the word appears).
+    @param term - [string] term to calculate the idf for.
+    @param tokenized_document - [list of list string] it can be document.
+    @return [int] value of the IDF.
+    '''
+    @staticmethod
+    def compute_IDF(term, tokenized_document):
+        doc_number=0
+        # Iterating the paragraphs.
+        for doc in tokenized_document:
+            if term in doc:
+                doc_number += 1
+        return math.log(len(tokenized_document/doc_number))
+
+
+    '''
+    Computes the TF-IDF value.
+    @param term - [string] the term to calculate the tf-idf value.
+    @param document - [list of string] document or array of docs that needs to be
+    calculated.
+    @return [int] - value of the computed Tf-Idf
+    '''
+    @staticmethod
+    def compute_TF_IDF(term, document):
+        tf = computeTF(term, document)
+        idf = computeIDF(term, document)
+        return tf * idf
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from nltk.corpus import cmudict, treebank
 from nltk.tag import UnigramTagger
 
 from paragraph import ParagraphAnalyser
+from sentence import SentenceAnalyser
 # load the resources
 
 
@@ -45,17 +46,32 @@ def main():
     train_sents = treebank.tagged_sents()[:5000]
     tagger = UnigramTagger(train_sents)
 
-    analyser = ParagraphAnalyser(corpusReader, tagger, stopWords)
-    feature_arr = analyser.compute_paragraph_features(corpus=corpusReader)
-    feature_arr = analyser.classify_chunks_paragraph(feature_dict=feature_arr,
+    para_analyser = ParagraphAnalyser(corpusReader, tagger, stopWords)
+    feature_arr = para_analyser.compute_paragraph_features(corpus=corpusReader)
+    feature_arr = para_analyser.classify_chunks_paragraph(feature_dict=feature_arr,
                                                     corpus=corpusReader)
-    print "\n\===============Data after feature classification===============\n"
-    pretty_printer.pprint(feature_arr)
+    # print "\n\===============Data after feature classification===============\n"
+    # pretty_printer.pprint(feature_arr)
 
+    sentence_analiyser = SentenceAnalyser(corpusReader, tagger, stopWords)
+    sent_feat_arr = sentence_analiyser.compute_sentence_features(corpusReader)
+    sent_feat_arr = sentence_analiyser.classify_chunks_sentence(sent_feat_arr, corpusReader)
+
+    para_ratio = 0
+    sent_ratio = 0
 
     for item in feature_arr:
         if "plagiarized_doc" in item:
-            print "\nDocument is plagiarised with a ratio of:"+ \
-            str(item["plagiarised_ratio"])
+            para_ratio = item["plagiarised_ratio"]
+    for item in sent_feat_arr:
+        if "plagiarized_sentence" in item:
+            sent_ratio = item["plagiarised_ratio"]
+
+    if sent_ratio != 0 and para_ratio != 0:
+        print "\nDocument is plagiarised with a ratio of:"+ \
+        str(float(para_ratio)+float(sent_ratio)/100.0)
+
+
+
 
 if __name__ == "__main__": main()
